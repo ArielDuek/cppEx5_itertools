ce733f5f82b2e581621f23cad6b7a38d5ddc77b7
TomLatin <tomlatin52@gmail.com>
2020-06-16 21:28:12 +0300

    pusg for ariel

--- accumulate.hpp
+++ accumulate.hpp
@@ -1,3 +1,50 @@
 namespace itertools{
+    template<class T >
+    class accumulate{
+    protected:
+        T container;
+    public:
+        accumulate(T c):container(c){}
 
+        class iterator {
+
+        private:
+            accumulate<T> acc;
+        public:
+            iterator(accumulate<T> a): acc(a){}
+
+            T& operator*() const {
+                //return *pointer_to_current_node;
+                return pointer_to_current_node->m_value;
+            }
+
+            T* operator->() const {
+                return &(pointer_to_current_node->m_value);
+            }
+
+            // ++i;
+            iterator& operator++() {
+                //++pointer_to_current_node;
+                pointer_to_current_node = pointer_to_current_node->m_next;
+                return *this;
+            }
+
+            // i++;
+            // Usually iterators are passed by value and not by const& as they are small.
+            const iterator operator++(int) {
+                iterator tmp= *this;
+                pointer_to_current_node= pointer_to_current_node->m_next;
+                return tmp;
+            }
+
+            bool operator==(const iterator& rhs) const {
+                return pointer_to_current_node == rhs.pointer_to_current_node;
+            }
+
+            bool operator!=(const iterator& rhs) const {
+                return pointer_to_current_node != rhs.pointer_to_current_node;
+            }
+        };  // END OF CLASS ITERATOR
+        
+    };
 }
